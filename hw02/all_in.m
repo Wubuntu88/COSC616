24fd6daed22295116404c0d3062f89ec9ab3a7e4
William Gillespie <wgilles1@emich.edu>
2016-09-17 18:06:51 -0400

    Update all_in.m

--- hw02/all_in.m
+++ hw02/all_in.m
@@ -1,7 +1,7 @@
 function res = all_in(a, b)
 for i = 1:length(a)
     matchingIndex = find(a(i)==b, 1, 'last')
-    if length(matchingIndex) != 1
+    if length(matchingIndex)!=1
         res = 0
         return
     end
