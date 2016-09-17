f0dfa538038dcb1b7fe2230456b6d7295396c736
William Gillespie <wgilles1@emich.edu>
2016-09-17 18:06:10 -0400

    Create all_in.m

---
+++ hw02/all_in.m
@@ -0,0 +1,11 @@
+function res = all_in(a, b)
+for i = 1:length(a)
+    matchingIndex = find(a(i)==b, 1, 'last')
+    if length(matchingIndex) != 1
+        res = 0
+        return
+    end
+    b(matchingIndex) = []
+end
+res = 1
+end
