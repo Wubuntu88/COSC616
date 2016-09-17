211474cffefceb548a70878a58cbc6e6ceb5d29f
William Gillespie <wgilles1@emich.edu>
2016-09-17 17:52:24 -0400

    Create remove.m

---
+++ hw02/remove.m
@@ -0,0 +1,8 @@
+function remain = removeElems(arr1, arr2)
+for i = 1:length(arr1)
+    indexToRemove = find(arr1(i)==arr2, 1, 'last');
+    if length(indexToRemove)
+        arr2(indexToRemove) = [];
+    end
+end
+end
