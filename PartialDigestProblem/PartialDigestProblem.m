b8ccf81fbd961c14a587f75213dd48a54fd78156
William Gillespie <wgilles1@emich.edu>
2016-09-17 20:02:19 -0400

    An initial attempt at the partial digest problem.  I must test and refactor it because I cannot execute the matlab code on my home computer.

---
+++ PartialDigestProblem/PartialDigestProblem.m
@@ -0,0 +1,29 @@
+function results = PDP(L)
+width = max(L)
+lastIndex = find(L == width, 1, 'last')
+L(lastIndex) = []
+X = [0, width]
+PLACE(X, L)
+end
+
+function res = PLACE(L, X):
+if length(L) == 0
+  fprintf(X)
+  return
+end
+
+%y is the max element in L
+y = max(L)
+differences1 = delta(y, X)
+if all_in(differences1, L)
+  new_X = sort([X y])
+  new_L = removeElems(differences1, L)
+  PLACE(new_X, new_L)
+
+width_minus_y = width - y
+differences2 = delta(width_minus_y, X)
+if all_in(differences2, L)
+  new_X = sort([X y])
+  new_L = removeElems(differences2, X)
+  PLACE(new_X, new_L)
+return
