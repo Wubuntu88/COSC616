e58e99344cbd58d7a15e25e6ba8f579315c5a962
William Gillespie <wgilles1@emich.edu>
2016-09-22 16:38:47 -0400

    Copied over from other directory.

---
+++ PartialDigestProblem/remove.m
@@ -0,0 +1,9 @@
+function remain = removeElems(arr1, arr2)
+for i = 1:length(arr1)
+    indexToRemove = find(arr1(i) == arr2, 1, 'last');
+    if isempty(indexToRemove) == false
+        arr2(indexToRemove) = [];
+    end
+    remain = arr2;
+end
+end
