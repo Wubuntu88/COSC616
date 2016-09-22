4d13b81d25d5fbb8a08d9128070742fab9ee0f70
William Gillespie <wgilles1@emich.edu>
2016-09-22 16:37:42 -0400

    Added printout statements for the trace and renamed the differences variables to deltas_1 and deltas_2.

--- PartialDigestProblem/Place.m
+++ PartialDigestProblem/Place.m
@@ -1,25 +1,26 @@
 function Place(L, X, level, iftrace)
 % base case
 if isempty(L)
+  output = strcat('solution found: ', 
+  fprintf('solution found: ')
   fprintf(X)
   return
 end
 
-%y is the max element in L
 y = max(L);
 deltas_1 = delta(y, X);
 if all_in(deltas_1, L)
-  new_X = sort([X y])
-  new_L = removeElems(deltas_1, L)
+  new_X = sort([X y]);
+  new_L = removeElems(deltas_1, L);
   Place(new_X, new_L)
 end
 
-width_minus_y = width - y
-deltas_2 = delta(width_minus_y, X)
+width_minus_y = width - y;
+deltas_2 = delta(width_minus_y, X);
 if all_in(deltas_2, L)
-  new_X = sort([X y])
-  new_L = removeElems(deltas_2, X)
-  Place(new_X, new_L)
+  new_X = sort([X y]);
+  new_L = removeElems(deltas_2, X);
+  Place(new_X, new_L);
 end
 return
 end
