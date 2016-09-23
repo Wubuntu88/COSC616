803c7db030e4875786f99b47c4ec786761ad5ec3
William Gillespie <wgilles1@cs-513-02.local>
2016-09-23 16:23:02 -0400

    Fixed a bug in the second if statement that does the recursive call.  I had added y to the X array where I should have added width_minus_y.  It is fixed now and printing correct output.

--- PartialDigestProblem/Place.m
+++ PartialDigestProblem/Place.m
@@ -1,7 +1,5 @@
 function Place(L, X, level, iftrace)
 % base case
-%l_str = sprintf('%d ', L);
-%fprintf('L: %s\n', l_str);
 if isempty(L)
   str = sprintf('%d ', X);
   output = strcat('solution found: ', str, '\n'); 
@@ -10,17 +8,7 @@ if isempty(L)
 end
 
 y = max(L);
-
-%x_str = sprintf('%d ', X);
-%fprintf('x: %s\n', x_str);
-
-
-%fprintf('%d\n', y);
-
 deltas_1 = abs(X - y);
-%s = sprintf('%d ', deltas_1);
-%fprintf('%s\n', s);
-
 if all_in(deltas_1, L)
   new_X = sort([X y]);
   new_L = remove(deltas_1, L);
@@ -30,7 +18,7 @@ end
 width_minus_y = max(X) - y;
 deltas_2 = abs(X - width_minus_y);
 if all_in(deltas_2, L)
-  new_X = sort([X y]);
+  new_X = sort([X width_minus_y]);
   new_L = remove(deltas_2, L);
   Place(new_L, new_X, 0, true);
 end
