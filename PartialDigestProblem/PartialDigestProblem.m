628e084888945e0df75c7251948d77be1e5a87fd
William Gillespie <wgilles1@emich.edu>
2016-09-23 16:58:56 -0400

    Made it so that the user can specify whether the program performes a trace.

--- PartialDigestProblem/PartialDigestProblem.m
+++ PartialDigestProblem/PartialDigestProblem.m
@@ -1,11 +1,10 @@
-function PartialDigestProblem(L, iftrace)
+function PartialDigestProblem(L,trace)
 width = max(L);
 lastIndex = find(L == width, 1, 'last');
 L(lastIndex) = [];
 X = [0, width];
 
 level = 0;
-iftrace = true;
-Place(L, X, level, iftrace)
+Place(L, X, width, level, trace)
 end
 
