4f741834192353191213b1f4b744b50b3da30026
William Gillespie <wgilles1@emich.edu>
2016-10-19 20:29:31 -0400

    Just removed some printout statements I made while debugging.

--- MotifFinding_1/profile.m
+++ MotifFinding_1/profile.m
@@ -1,11 +1,8 @@
 function p = profile(s, DNA, l_mer)
-
+%fprintf('-------------------------\n');
 for i = 1:length(s)
     position = s(i);
-    
-    fprintf('position1: %2d\n', position);
-    fprintf('position2: %2d\n', position+l_mer-1);
-    fprintf('lmer: %2d\n', l_mer);
+    [t, n] = size(DNA);
     temp = DNA(i,position:position+l_mer-1);
     p(i,:) = actg2num(temp);
 end
